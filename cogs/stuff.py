712845263fcad371058af8d8258ec1ed1c5a1483
xcdm <xcdmcode@gmail.com>
2020-03-26 14:38:22 -0300

    Added juan command

--- cogs/stuff.py
+++ cogs/stuff.py
@@ -1,4 +1,5 @@
 import discord
+from discord import Colour, Embed
 from discord.ext import commands
 
 class StuffCog(commands.Cog, name="Media Commands"):
@@ -8,6 +9,19 @@ class StuffCog(commands.Cog, name="Media Commands"):
     @commands.command(name='ao')
     async def sneed(self, ctx):
         await ctx.send("sneed")
+
+    @commands.command(name='juan', aliases=['simp'])
+    async def juan(self, ctx):
+        embed = Embed()
+        embed.type = "rich"
+        embed.colour = Colour.blue()
+        embed.set_image(
+            url="https://cdn.discordapp.com/attachments/160802211742482433/692784652297044058/LMAO.png"
+        )
+        embed.set_footer(
+            text="COPERIGHTED. ALL RIGHTS RESERVED"
+        )
+        await ctx.send(None, embed=embed)
 
 def setup(bot):
     bot.add_cog(StuffCog(bot))
